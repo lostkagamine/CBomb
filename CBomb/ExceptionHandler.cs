e4fd4aa965f87916096d12e1a7c7b674e415a01d
ry00001 <15631667+ry00001@users.noreply.github.com>
2018-06-16 18:14:38 +0100

    Initial commit

---
+++ CBomb/ExceptionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBomb
+{
+	namespace ExceptionHandler
+	{
+		class Crasher
+		{
+			public void Crash(Exception e)
+			{
+				System.Windows.Forms.MessageBox.Show(e.ToString(), "don't you love it when your virus crashes", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+				Environment.Exit(1);
+			}
+		}
+	}
+}
